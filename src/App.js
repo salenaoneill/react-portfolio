4a53dc3ea596c60139c571de814720d2acc37a2c
Salena O'Neill <salenaoneill@salenas-air.lan>
2023-03-22 10:30:37 -0500

    contact

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from "react";
 import PortfolioContainer from "./components/PortfolioContainer";
 
-import allProjects from './allProjects';
-
 const App = () => <PortfolioContainer />;
 
 export default App;
--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,15 +1,49 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import emailjs from "@emailjs/browser";
 
-function Contact() {
+const Contact = () => {
+    function sendEmail(e) {
+        e.preventDefault();
+        emailjs.sendForm(
+            "service_blfoi0v",
+            "template_4iuqxw8",
+            e.target,
+            "3M-Gmkf0pl6oh03Mv"
+        )
+        .then(
+            (result) => {
+                    console.log(result.text);
+                    alert("Your email has been successfully sent");
+            },
+            (error) => {
+                console.log(error.text);
+                alert("Something went wrong");
+            }
+        )
+        
+    }
+
     return (
-        <div>
-            <h2>Contact Me!</h2>
-            <div>
-                <textarea>Name:</textarea>
-                <textarea>Email address:</textarea>
-                <textarea>Message:</textarea>
-            </div>
-        </div>
+        <form onSubmit={sendEmail}>
+                <h2>Contact Me!</h2>
+                <div>
+                    <label>Name:</label>
+                    <textarea
+                        type="text"
+                        className="Name"
+                        required>
+                    </textarea>
+                    <label>Email Address:</label>
+                    <textarea
+                        type="text"
+                        className="EmailAddress"
+                        required>
+                    </textarea>
+                    <label>Message:</label>
+                    <textarea type="text" className="message" required></textarea>
+                    <button type="submit">Submit</button>
+                </div>
+        </form>
     )
 }
 
