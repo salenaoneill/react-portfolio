cd2af34f61819a4b353be43920c6c8e275cfc90f
Salena O'Neill <salenaoneill@salenas-air.lan>
2023-03-27 17:35:24 -0500

    styling& final touches

--- src/allProjects.js
+++ src/allProjects.js
@@ -1,31 +1,52 @@
+import Evenster from './/images/Evenster.png';
+import myAnimalList from './/images/my-animal-list.png';
+import codeQuiz from './/images/CodeQuiz.png';
+import weatherDashboard from './/images/weather-dashboard.png';
+import workDayScheduler from './/images/work-day-scheduler.png';
+import noteTaker from './/images/Note-Taker.png';
+
 const allProjects = [
     {
         id: 1,
         name: 'Evenster',
-        image: 'h',
+        image: Evenster,
         deployedLink: 'https://salenaoneill.github.io/eventster/',
         gitHubLink: 'https://github.com/salenaoneill/eventster'
     }, 
     {
         id: 2,
         name: 'My Animal List',
-        image: 'a', 
-        depoyedLink: 'https://my-animal-list.herokuapp.com/',
+        image: myAnimalList, 
+        deployedLink: 'https://my-animal-list.herokuapp.com/',
         gitHubLink: 'https://github.com/salenaoneill/my-animal-list'
     },
     {
         id: 3,
         name: 'Code Quiz',
-        image: 'b',
+        image: codeQuiz,
         deployedLink: 'https://salenaoneill.github.io/code-quiz/',
         gitHubLink: 'https://github.com/salenaoneill/code-quiz'
     },
     {
         id: 4,
         name: 'Weather Dashboard',
-        image: 'c',
+        image: weatherDashboard,
         deployedLink: 'https://salenaoneill.github.io/weather-dashboard/',
         gitHubLink: 'https://github.com/salenaoneill/weather-dashboard'
+    },
+    {
+        id: 5,
+        name: 'Work Day Scheduler',
+        image: workDayScheduler,
+        deployedLink: 'https://salenaoneill.github.io/work-day-scheduler/Develop/',
+        gitHubLink: 'https://github.com/salenaoneill/work-day-scheduler'
+    },
+    {
+        id: 6,
+        name: 'Note Taker',
+        image: noteTaker,
+        deployedLink: 'https://notestaker.herokuapp.com/',
+        gitHubLink: 'https://github.com/salenaoneill/note-taker'
     }
 ];
 
--- src/components/pages/AboutMe.js
+++ src/components/pages/AboutMe.js
@@ -1,9 +1,9 @@
 import React from 'react';
-
+import PortfolioPic from '../../images/portfolio-pic.jpeg';
 function AboutMe() {
     return (
         <div>
-            <img src="../../../images/portfolio-pic.jpeg"></img>
+            <img src={PortfolioPic}></img>
             <p>
                 My name is Salena and I am currently a boot camp student! 
                 I am almost a full-stack developer and I look forward to beginning the
